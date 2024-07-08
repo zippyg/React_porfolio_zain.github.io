07049b315f5577a34b8e18fd9d699d5774a84fec
zain <zainmughal77@outlook.com>
2024-07-08 13:47:42 +0100

    try linked

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://zippyg.github.io/React_porfolio_zain.github.io/',
+  homepage: 'https://zippyg.github.io/React_porfolio_zain.github.io',
   title: '(Mohammed) Zain Mughal.',
 };
 
